85af62588ac1583d892e08165c65a04e93d90eb1
abhie-lp <abhie.lp@gmail.com>
2024-05-20 21:47:34 +0530

    Add option to copy latest file or the file specified.

--- newnext.py
+++ newnext.py
@@ -2,6 +2,7 @@
 
 import os
 import re
+import shutil
 from argparse import ArgumentParser
 from glob import glob
 
@@ -12,12 +13,17 @@ parser = ArgumentParser(
 )
 
 parser.add_argument("name", help="Name of the file/folder to create.")
+parser.add_argument(
+    "-c", "--copy", action="store", const="", nargs="?", type=str,
+    help="By default copy the latest file or else the file given"
+)
 
 args = parser.parse_args()
 current_dir = os.path.abspath(os.path.curdir)
 files = glob(f"{args.name}[\\.-_]*", )
+latest_file = None
 if files:
-    latest_file = max(files, key=os.path.getctime)
+    latest_file = max(files)
     escaped_basename = re.escape(args.name)
     sequence = re.search(rf"{escaped_basename}([-._]?)(\d+).*", latest_file)
     if sequence:
@@ -29,8 +35,14 @@ if files:
     new_file = new_file + "." + latest_file.rsplit(".", 1)[1]
 else:
     new_file = args.name + DEFAULT_SEQUENCE + ".txt"
-try:
+
+if args.copy is not None and latest_file:
+    if args.copy == "":
+        shutil.copy(latest_file, new_file)
+    elif os.path.isfile(args.copy):
+        shutil.copy(args.copy, new_file)
+    else:
+        parser.error(f"Given '{args.copy}' file to copy is not a file.")
+else:
     open(new_file, "a").close()
-    print(os.path.join(current_dir, new_file))
-except Exception as e:
-    print(e)
+print(os.path.join(current_dir, new_file))
