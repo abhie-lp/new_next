4f4b31f631f0d6a8caace77fa8210017b79a0f75
abhie-lp <abhie.lp@gmail.com>
2024-05-20 12:29:28 +0530

    Print absolute path of new file

--- newnext.py
+++ newnext.py
@@ -2,7 +2,6 @@
 
 import os
 import re
-import sys
 from argparse import ArgumentParser
 from glob import glob
 
@@ -15,28 +14,23 @@ parser = ArgumentParser(
 parser.add_argument("name", help="Name of the file/folder to create.")
 
 args = parser.parse_args()
+current_dir = os.path.abspath(os.path.curdir)
 files = glob(f"{args.name}[\\.-_]*", )
-if not files:
-    try:
-        filename = args.name + DEFAULT_SEQUENCE + ".txt"
-        open(filename, "a").close()
-        print(filename)
-        sys.exit(0)
-    except Exception as e:
-        print(e)
-
-latest_file = max(files, key=os.path.getctime)
-escaped_basename = re.escape(args.name)
-sequence = re.search(rf"{escaped_basename}([-._]?)(\d+).*", latest_file)
-if sequence:
-    separator, sequence = sequence.group(1), sequence.group(2)
-    new_file = (args.name + separator +
-                str(int(sequence) + 1).zfill(len(sequence)))
+if files:
+    latest_file = max(files, key=os.path.getctime)
+    escaped_basename = re.escape(args.name)
+    sequence = re.search(rf"{escaped_basename}([-._]?)(\d+).*", latest_file)
+    if sequence:
+        separator, sequence = sequence.group(1), sequence.group(2)
+        new_file = (args.name + separator +
+                    str(int(sequence) + 1).zfill(len(sequence)))
+    else:
+        new_file = args.name + "_002"
+    new_file = new_file + "." + latest_file.rsplit(".", 1)[1]
 else:
-    new_file = args.name + "_002"
-new_file = new_file + "." + latest_file.rsplit(".", 1)[1]
+    new_file = args.name + DEFAULT_SEQUENCE + ".txt"
 try:
-    open(new_file, "w").close()
-    print(new_file)
+    open(new_file, "a").close()
+    print(os.path.join(current_dir, new_file))
 except Exception as e:
     print(e)
